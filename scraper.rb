45f48a56aef89790f32366d02064c327297b666b
Harold Seah <harryseahjy@gmail.com>
2021-02-23 16:11:53 +0800

    Added text find function

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,11 @@
-# select text
-# extract text
-# reproduce text
+# take text
+test = "popopopopopopopoabcpopopopopopopopopop"
+# locates query in text
+# get query
+p "What are you looking for?"
+query = gets.chomp
+p query
+# check text
+# answers query
+
+
