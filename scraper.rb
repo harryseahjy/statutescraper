f09515ac796bbd35d40ec5faafbb16422b9162a0
Harold Seah <harryseahjy@gmail.com>
2021-02-23 16:39:36 +0800

    Added basic functionality to obtain user input and to give response.

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,15 @@
 # take text
-test = "popopopopopopopoabcpopopopopopopopopop"
+test_text = "popopopopopopopoabcpopopopopopopopopop"
 # locates query in text
 # get query
-p "What are you looking for?"
-query = gets.chomp
-p query
+p 'What are you looking for?'
+search = gets.chomp
+p "You searched for: #{search}." 
 # check text
+def find(text, query)
+    #boolean function -- see if string exists
+    result = text.match("#{query}") 
+    result == nil ? "No match" : result
+end
+p find(test_text, search)
 # answers query
-
-
